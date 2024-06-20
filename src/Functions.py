f3bcf04651daa7ea33505d7b97be4d905f983e30
Leander Wollersberger <leander.knampf@gmx.at>
2024-06-20 14:33:23 +0200

    Half-functioning code from starting point to picking up the first four blocks

--- src/Functions.py
+++ src/Functions.py
@@ -1,19 +1,18 @@
-import Functions
 import Motors
 import State
-import Main
+import Roboter
 from pybricks.parameters import Axis, Button, Color, Direction, Port, Side, Stop
 from pybricks.tools import wait
 
 def calibrateHolder():
-    Motors.carrier_motor.run_time(-1000, 1000, Stop.HOLD, True)
+    # Motors.carrier_motor.run_time(-1000, 1000, Stop.HOLD, True)
     Motors.pickup_motor.run_until_stalled(-1000, Stop.HOLD, 80)
     State.holderOpenAngle = 0
     Motors.pickup_motor.reset_angle(0)
     return
 
 def calibrateCarrier():
-
+    # Letting the carrier fall down doesn't work. This is useless
     Motors.carrier_motor.run_time(-1000, 1000, Stop.HOLD, True)
     Motors.carrier_motor.stop()
     wait(100) # Leave time so the carrier falls to the ground
@@ -23,12 +22,11 @@ def calibrateCarrier():
     return
 
 def moveCarrier(heightToMove):
-    degressPerHeightFactor = 36 # 36 degress / mm
+    degressPerHeightFactor = 34 # 34 degress / mm
 
     rotationAngle = degressPerHeightFactor * heightToMove
 
-    # TODO: does positive speed + negative angle create a negative rotation?
-    Motors.carrier_motor.run_angle(1000, rotationAngle, Stop.HOLD)
+    Motors.carrier_motor.run_angle(-1000, rotationAngle, Stop.HOLD)
     
     State.carrierHight += heightToMove
 
@@ -41,11 +39,11 @@ def moveCarrierByBlocks(blocksToMove):
 # Carrier is above the blocks and should pick them up
 def pickUpBlocks():
     moveCarrier(-State.carrierHight)
-    moveCarrier(10)
+    moveCarrier(20)
     return
 
 def driveForwardToBlockAndPickUp(distance, numOfBlocks):
     moveCarrierByBlocks(numOfBlocks)
-    Main.driveBase.straight(distance)
+    Roboter.driveBase.straight(distance)
     pickUpBlocks()
     return
--- src/GameRoutine.py
+++ src/GameRoutine.py
@@ -1,29 +1,53 @@
 import Functions
 import Motors
 import State
-import Main
+import Roboter
+from pybricks.tools import wait
+from pybricks.parameters import Axis, Button, Color, Direction, Port, Side, Stop
 
 def enum(**enums: int):
     return type('Enum', (), enums)
 
-GameState = enum(CALIBRATION=0, FIRSTBLOCKS_PICKUP=1, FIRSTBLOCKS_READCOLOURS=2)
-
+GameState = enum(CALIBRATION=0, FIRSTBLOCKS_PICKUP=1, FIRSTBLOCKS_READCOLOURS=2, DEAD=3, TEST=4, MOVETOFIRSTBLOCKS=5)
 state = GameState.CALIBRATION
 
 def gameRoutine():
 
+    global GameState
+    global state
+
+    if state == GameState.TEST:
+        # Functions.calibrateHolder()
+        # Roboter.driveBase.curve(-200, -180)
+        Roboter.driveBase.straight(-500)
+        state = GameState.DEAD
+        return
+
     if state == GameState.CALIBRATION:
+        Functions.moveCarrier(20)
         Functions.calibrateHolder()
-        Functions.calibrateCarrier()
-        Functions.moveCarrier(10)
+        # Functions.calibrateCarrier()
+        # wait(1000000)
+        state=GameState.MOVETOFIRSTBLOCKS
+        return
+    
+    elif state == GameState.MOVETOFIRSTBLOCKS:
+        Roboter.driveBase.turn(-50)
+        Roboter.driveBase.straight(115)
+        Roboter.driveBase.turn(70, Stop.COAST)
+        Functions.moveCarrierByBlocks(1)
+        Roboter.driveBase.straight(-252, Stop.COAST)
+        # Roboter.driveBase.turn(-5, Stop.HOLD)
         state=GameState.FIRSTBLOCKS_PICKUP
+        # state=GameState.DEAD
         return
 
     elif state == GameState.FIRSTBLOCKS_PICKUP:
-        Main.driveBase.curve(500, 20) # This probably doesn't work out
-        Functions.driveForwardToBlockAndPickUp(50, 1)
+        # Roboter.driveBase.curve(500, 20) # This probably doesn't work out
+        # Functions.driveForwardToBlockAndPickUp(-100, 1)
+        Functions.pickUpBlocks()
         for i in range(3):
-            Functions.driveForwardToBlockAndPickUp(50, 1)
+            Functions.driveForwardToBlockAndPickUp(-100, 1)
         state=GameState.FIRSTBLOCKS_READCOLOURS
         return
     
